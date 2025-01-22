8bee78d3641b1f20f3968b5c970900535e340c75
Srijan1202 <srijan.srivastava1202@gmail.com>
2025-01-22 09:36:41 +0530

    middleware

--- index.js
+++ index.js
@@ -3,12 +3,23 @@ const app = express()
 const path=require('path')
 const port = 3000
 
-app.get('/', (req, res) => {
+const srijanmiddleware = (req,res,next) => {
+    console.log(req)
+    next()
+}
+
+
+app.use(express.static(path.join(__dirname,'public')))
+app.use(srijanmiddleware)
+
+app.get('/helloworld', (req, res) => {
   res.send('Hello World!')
 })
 
 app.get('/about', (req, res) => {
-    res.sendFile(path.join(__dirname, 'index.html'))
+    // res.sendFile(path.join(__dirname, 'index.html'))
+    // res.status(500)
+    res.json({"harry":34})
   })
 
 app.listen(port, () => {
